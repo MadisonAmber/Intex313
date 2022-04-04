d330a6a907302f3c47dc5dff63136e97d65fe83c
Madison Clark <madamber4@gmail.com>
2022-04-04 09:56:46 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Intex313.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
