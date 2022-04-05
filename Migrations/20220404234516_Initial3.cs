15ffa65c39590e3dba9fc7f32d57016fb2210bdb
Madison Clark <madamber4@gmail.com>
2022-04-04 19:30:32 -0600

    lots of changes

---
+++ Migrations/20220404234516_Initial3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intex313.Migrations
+{
+    public partial class Initial3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Main_Road_Name",
+                table: "Accidents",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Main_Road_Name",
+                table: "Accidents",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
