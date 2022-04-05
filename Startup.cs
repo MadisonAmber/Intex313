c5fd5a8c0236b766b05ecdefa27bd55bb5117acf
dallinks <dallinstirling@gmail.com>
2022-04-05 11:43:46 -0600

    pre-identity dbcontext test

--- Startup.cs
+++ Startup.cs
@@ -58,7 +58,7 @@ namespace Intex313
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
