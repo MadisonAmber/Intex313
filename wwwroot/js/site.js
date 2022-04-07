0f4c17375dd4cfdcc64354b702cb282ace2924f7
dallinks <dallinstirling@gmail.com>
2022-04-07 13:36:55 -0600

    Google Maps API

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function initMap(longitude, latitude) {
+    const location = { lat: latitude, lng: longitude };
+    const map = new google.maps.Map(document.getElementById("map").{
+        zoom: 4,
+        center: location,
+    });
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+}
