63a103c07d22798f9d240cd5df9d78fc6b1f7c5b
dallinks <dallinstirling@gmail.com>
2022-04-07 18:06:42 -0600

    Password Length

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace Intex313
             });
 
             services.AddControllersWithViews();
-
+            
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddDefaultTokenProviders()
                 .AddDefaultUI()
@@ -64,7 +64,10 @@ namespace Intex313
             //Repository method
             //services.AddScoped<IAccidentRepository, EFAccidentRepository>();
             services.AddRazorPages();
-
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 8;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,14 +83,13 @@ namespace Intex313
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
 
-
             app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseCookiePolicy();
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
-
+            
             app.UseXXssProtection(options => options.EnabledWithBlockMode());
             app.UseCsp(opts => opts
                 .BlockAllMixedContent()
